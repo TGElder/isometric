b8bdd23861afc39db1d1d2891c03c14b7f70694f
Thomas Elder <thomas.elder@gmail.com>
2019-02-06 20:11:56 +0000

    Mouse scrolling

--- src/engine.rs
+++ src/engine.rs
@@ -9,12 +9,8 @@ use std::marker::PhantomData;
 pub struct IsometricEngine {
     events_loop: glutin::EventsLoop,
     window: glutin::GlWindow,
-    vao: VAO<TriangleBuffer>,
-    vertex_count: i32,
-    program: Program,
-    scale: f32,
-    translation: (f32, f32),
-    isometric_matrix: na::Matrix4<f32>
+    graphics: GraphicsEngine,
+    drag_controller: DragController,
 }
 
 impl IsometricEngine {
@@ -35,12 +31,126 @@ impl IsometricEngine {
             gl_window.make_current().unwrap();
         }
 
+        let graphics = GraphicsEngine::new(&gl_window, vertices);
+
+        IsometricEngine {
+            events_loop,
+            window: gl_window,
+            graphics,
+            drag_controller: DragController::new(),
+        }
+      
+    }
+
+    pub fn run(&mut self) {        
+        let mut running = true;
+        while running {
+            
+            let graphics = &mut self.graphics;
+            let events_loop = &mut self.events_loop;
+            let window = &self.window;
+            let drag_controller = &mut self.drag_controller;
+            let window_size = self.window.window().get_inner_size().unwrap();
+            events_loop.poll_events(|event| {
+                match event {
+                    glutin::Event::WindowEvent{ ref event, .. } => {
+                        match event {
+                            glutin::WindowEvent::CloseRequested => running = false,
+                            glutin::WindowEvent::Resized(logical_size) => {
+                                let dpi_factor = window.get_hidpi_factor();
+                                window.resize(logical_size.to_physical(dpi_factor));
+                            },
+                            glutin::WindowEvent::MouseWheel{ delta, .. } =>  match delta {
+                                glutin::MouseScrollDelta::LineDelta(_, d) if *d > 0.0 => graphics.scale(2.0),
+                                glutin::MouseScrollDelta::LineDelta(_, d) if *d < 0.0 => graphics.scale(0.5),
+                                _ => ()
+                            },
+                            _ => ()
+                        };
+                        
+                    },
+                    _ => ()
+                };
+                if let Some((x, y)) = drag_controller.handle(event) {
+                    graphics.translate(((x / window_size.width) as f32, (y / window_size.height) as f32));
+                }
+            }
+            );
+            
+
+            graphics.draw();
+
+            self.window.swap_buffers().unwrap();            
+
+        }
+    }
+
+  
+
+    
+
+}
+
+pub struct DragController {
+    dragging: bool,
+}
+
+impl DragController {
+
+    fn new() -> DragController {
+        DragController{
+            dragging: false,
+        }
+    }
+
+    fn handle(&mut self, event: glutin::Event) -> Option<(f64, f64)> {
+        match event {
+            glutin::Event::WindowEvent{ event, .. } => match event {
+                glutin::WindowEvent::MouseInput{ state, button: glutin::MouseButton::Left, .. } => {
+                    match state {
+                        glutin::ElementState::Pressed => self.dragging = true,
+                        glutin::ElementState::Released => self.dragging = false,
+                    };
+                    None
+                },
+                _ => None
+            },
+            glutin::Event::DeviceEvent{ event, .. } => match event {
+                glutin::DeviceEvent::MouseMotion{ delta } => {
+                    if self.dragging {
+                        Some(delta)
+                    } else {
+                        None
+                    }
+                },
+                _ => None
+            },
+            _ => None
+        }
+    }
+}
+
+pub struct GraphicsEngine {
+    vao: VAO<TriangleBuffer>,
+    vertex_count: i32,
+    program: Program,
+    scale: f32,
+    translation: (f32, f32),
+    isometric_matrix: na::Matrix4<f32>,
+}
+
+impl GraphicsEngine {
+
+    pub fn new(gl_window: &glutin::GlWindow, vertices: Vec<f32>) -> GraphicsEngine {
+
+        let window_size: (u32, u32) = gl_window.window().get_inner_size().unwrap().into();
+
         unsafe {
             gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
-            gl::Viewport(0, 0, width as i32, height as i32);
+            gl::Viewport(0, 0, window_size.0 as i32, window_size.1 as i32);
             gl::ClearColor(0.0, 0.0, 1.0, 1.0);
         }
-        
+
         let vao: VAO<TriangleBuffer> = VAO::new();
         let vbo: VBO = VBO::new(&vao);
 
@@ -48,11 +158,9 @@ impl IsometricEngine {
 
         vbo.load(vertices);
 
-        let program = IsometricEngine::load_program();
+        let program = GraphicsEngine::load_program();
 
-        IsometricEngine{
-            events_loop,
-            window: gl_window,
+        let out = GraphicsEngine{
             vao,
             vertex_count: vertex_count as i32,
             program,
@@ -61,77 +169,13 @@ impl IsometricEngine {
             isometric_matrix: na::Matrix4::new(
                 1.0, -1.0, 0.0, 0.0,
                 -0.5, -0.5, 1.0, 0.0,
-                0.5, 0.5, -1.0, 0.0,
+                0.0, 0.0, 0.0, 1.0,
                 0.0, 0.0, 0.0, 1.0),
-        }
-    }
-    
-    fn recompute_transform_matrix(&self) {
-        let scale_matrix: na::Matrix4<f32> = na::Matrix4::new(
-            self.scale, 0.0, 0.0, self.translation.0,
-            0.0, self.scale, 0.0, self.translation.1,
-            0.0, 0.0, self.scale, 0.0,
-            0.0, 0.0, 0.0, 1.0);
+        };
 
-        let composite_matrix = self.isometric_matrix * scale_matrix;
+        out.recompute_transform_matrix();
 
-        self.program.load_matrix("transform", composite_matrix);
-    }
-
-    pub fn scale(&mut self, scale: f32) {
-        self.scale = scale;
-        println!("{}", scale);
-        self.recompute_transform_matrix();
-    }
-
-    pub fn run(&mut self) {
-        let mut i = 0;
-        
-        let mut running = true;
-        while running {
-            let scale = self.scale;
-            let mut new_scale: Option<f32> = None;
-            {
-                let events_loop = &mut self.events_loop;
-                let window = &self.window;
-                events_loop.poll_events(|event| {
-                    match event {
-                        glutin::Event::WindowEvent{ event, .. } => match event {
-                            glutin::WindowEvent::CloseRequested => running = false,
-                            glutin::WindowEvent::Resized(logical_size) => {
-                                let dpi_factor = window.get_hidpi_factor();
-                                window.resize(logical_size.to_physical(dpi_factor));
-                            },
-                            glutin::WindowEvent::MouseWheel{ delta, .. } =>  match delta {
-                                glutin::MouseScrollDelta::LineDelta(_, d) if d > 0.0 => new_scale = Some(scale / 2.0),
-                                glutin::MouseScrollDelta::LineDelta(_, d) if d < 0.0 => new_scale = Some(scale * 2.0),
-                                _ => ()
-                            },
-                            _ => ()
-                        },
-                        _ => ()
-                    }
-                });
-            }
-
-            if let Some(value) = new_scale {
-                self.scale(value);
-            }
-
-            unsafe {
-                gl::Clear(gl::COLOR_BUFFER_BIT);
-                self.vao.bind();
-                gl::DrawArrays(
-                    gl::TRIANGLES, // mode
-                    0, // starting index in the enabled arrays
-                    self.vertex_count // number of indices to be rendered
-                );
-                self.vao.unbind();
-            }
-
-            self.window.swap_buffers().unwrap();
-            i = i + 1;
-        }
+        out
     }
 
     fn load_program() -> Program {
@@ -154,7 +198,44 @@ impl IsometricEngine {
         return shader_program;
     }
 
+    fn recompute_transform_matrix(&self) {
+        let scale_matrix: na::Matrix4<f32> = na::Matrix4::new(
+            self.scale, 0.0, 0.0, self.translation.0,
+            0.0, self.scale, 0.0, self.translation.1,
+            0.0, 0.0, self.scale, 0.0,
+            0.0, 0.0, 0.0, 1.0);
 
+        let composite_matrix = scale_matrix * self.isometric_matrix;
+
+        self.program.load_matrix("transform", composite_matrix);
+    }
+
+    pub fn scale(&mut self, delta: f32) {
+        self.scale = self.scale * delta;
+        self.recompute_transform_matrix();
+    }
+
+     pub fn translate(&mut self, delta: (f32, f32)) {
+        self.translation = (
+            self.translation.0 + delta.0,
+            self.translation.1 - delta.1,
+        );
+        self.recompute_transform_matrix();
+    }
+
+    pub fn draw(&self) {
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            self.vao.bind();
+            gl::DrawArrays(
+                gl::TRIANGLES, // mode
+                0, // starting index in the enabled arrays
+                self.vertex_count // number of indices to be rendered
+            );
+            self.vao.unbind();
+        }
+        
+    }
 
 }
 
