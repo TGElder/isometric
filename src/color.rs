e3409df5ad58c87c1ebf19725c14531996ba9cb7
Thomas Elder <tgelder@gmail.com>
2019-03-26 18:14:13 +0000

    Adding terrain drawing with matrix coloring

--- src/color.rs
+++ src/color.rs
@@ -10,4 +10,13 @@ impl Color {
     pub fn new(r: f32, g: f32, b: f32, a: f32) -> Color {
         Color{r, g, b, a}
     }
+
+    pub fn mul(&self, other: &Color) -> Color {
+        Color::new(
+            self.r * other.r,
+            self.g * other.g,
+            self.b * other.b,
+            self.a * other.a,
+        )
+    }
 }
--- src/graphics/drawing/terrain.rs
+++ src/graphics/drawing/terrain.rs
@@ -3,7 +3,7 @@ use ::color::Color;
 use super::super::vertex_objects::{VBO, ColoredVertex};
 use super::utils::*;
 use ::terrain::{Node, Edge, Terrain};
-use ::v2;
+use ::{v2, M};
 
 pub struct NodeDrawing {
     vbo: VBO<ColoredVertex>,
@@ -88,7 +88,31 @@ impl Drawing for TerrainDrawing {
 }
 
 impl TerrainDrawing {
-    pub fn new(terrain: &Terrain, coloring: Box<SquareColoring>) -> TerrainDrawing {
+    pub fn from_matrix(terrain: &Terrain, color_matrix: &M<Color>, shading: &Box<SquareColoring>) -> TerrainDrawing {
+
+        let mut vbo = VBO::new(gl::TRIANGLES);
+
+        let mut vertices = vec![];
+
+        for x in 0..((terrain.width() - 1) / 2) {
+            for y in 0..((terrain.height() - 1) / 2) {
+                let tile_index = v2(x, y);
+                let grid_index = Terrain::get_index_for_tile(&tile_index);
+                let border = terrain.get_border(grid_index);
+                let shade = shading.get_colors(&[border[0], border[1], border[2], border[3]])[0];
+                let color = color_matrix[(x, y)].mul(&shade);
+                for triangle in terrain.get_triangles_for_tile(&tile_index) {
+                    vertices.append(&mut get_uniform_colored_vertices_from_triangle(&triangle, &color));
+                }
+            }
+        }
+
+        vbo.load(vertices);
+
+        TerrainDrawing{vbo}
+    }
+
+    pub fn uniform(terrain: &Terrain, coloring: Box<SquareColoring>) -> TerrainDrawing {
 
         let mut vbo = VBO::new(gl::TRIANGLES);
 
