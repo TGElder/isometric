5582e5d3efc80535d81977de281bb3f905a24aac
Thomas Elder <tgelder@gmail.com>
2019-05-05 20:46:56 +0100

    Fixing multisamping sprite artefact

--- src/graphics/texture.rs
+++ src/graphics/texture.rs
@@ -40,6 +40,8 @@ impl Texture {
 
         unsafe {
             self.bind();
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);
             gl::TexImage2D(
